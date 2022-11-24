68481c89aab815c2b87b047993bdf9c60b412d7a
Pete Laptop <pmugasa78@gmail.com>
2022-11-24 15:50:12 +0200

    fixed the async code and added the await to the fetch and response

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
+//get user input
+const userLocation = prompt("enter your location");
+
 //getting the weather data
 
 async function getWeatherData() {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    userLocation +
+    "&appid=7ad09c03e347e967e0ff9126b62b8d0f";
 
-    const url = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=7ad09c03e347e967e0ff9126b62b8d0f"
-    let response = await fetch(url)
-    let data = await response.json()
-    console.log(data)
+  let response = await fetch(url, { mode: "cors" });
+  let data = await response.json();
+  console.log(data);
 }
 
 getWeatherData();
