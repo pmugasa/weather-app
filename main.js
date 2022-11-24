db73510639ec9f525cc2701ae26be7027d31225f
Pete Laptop <pmugasa78@gmail.com>
2022-11-24 16:21:57 +0200

    added the functionality so that user is only shown the actual temparature and how it feels like

--- main.js
+++ main.js
@@ -7,11 +7,17 @@ async function getWeatherData() {
   const url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     userLocation +
-    "&appid=7ad09c03e347e967e0ff9126b62b8d0f";
+    "&units=metric&appid=7ad09c03e347e967e0ff9126b62b8d0f";
 
-  let response = await fetch(url, { mode: "cors" });
-  let data = await response.json();
-  console.log(data);
+  try {
+    let response = await fetch(url, { mode: "cors" });
+    let data = await response.json();
+    console.log("temperature", data.main.temp);
+    console.log("feels like", data.main.feels_like);
+    console.log("response status:", response.status);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 getWeatherData();
